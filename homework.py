ee712317228eec0f8a8662cec7a7052e72b7f842
Noskov Alexey <vokson@yandex.ru>
2020-09-09 06:06:09 +0300

    Ready for 1st review

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import requests
@@ -6,19 +7,35 @@ from twilio.rest import Client
 
 def get_status(user_id):
     params = {
-        ...
+        'v': '5.122',
+        'access_token': os.getenv('VK_TOKEN'),
+        'user_ids': user_id,
+        'fields': 'online'
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    response = requests.post(
+        'https://api.vk.com/method/users.get',
+        params=params
+    ).json().get('response')
+
+    if not response:
+        return 0
+
+    return response[0]['online']
 
 
 def sms_sender(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+    client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_TOKEN'))
+
+    message = client.messages.create(
+            body=sms_text,
+            from_=os.getenv('NUMBER_FROM'),
+            to=os.getenv('NUMBER_TO')
+        )
+    return message.sid
 
 
-if __name__ == "__main__":
-    vk_id = input("Введите id ")
+if __name__ == '__main__':
+    vk_id = input('Введите id ')
     while True:
         if get_status(vk_id) == 1:
             sms_sender(f'{vk_id} сейчас онлайн!')
