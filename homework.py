d6facd295749c83f4de7d32982eafc46ba142f43
Noskov Alexey <vokson@yandex.ru>
2020-11-12 19:21:33 +0300

    Improved after 1st review

--- homework.py
+++ homework.py
@@ -1,31 +1,68 @@
+import logging
 import os
+import sys
 import time
 
 import requests
+from dotenv import load_dotenv
 from twilio.rest import Client
+
+
+load_dotenv()
+
+VERSION = '5.122'
+BASE_URL = 'https://api.vk.com/method'
+
+logging.basicConfig()
+log = logging.getLogger('VK_SMS_APP')
+log.setLevel(logging.INFO)
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+log.addHandler(stdout_handler)
 
 
 def get_status(user_id):
     params = {
-        'v': '5.122',
+        'v': VERSION,
         'access_token': os.getenv('VK_TOKEN'),
         'user_ids': user_id,
         'fields': 'online'
     }
-    response = requests.post(
-        'https://api.vk.com/method/users.get',
-        params=params
-    ).json().get('response')
+    url = '{}/{}'.format(BASE_URL, 'users.get')
 
-    if not response:
+    try:
+        response = requests.post(url, params=params)
+
+    except requests.exceptions.RequestException:
+        log.exception('Error while handling request')
         return 0
 
-    return response[0]['online']
+    except Exception:
+        log.exception('Unknown exception')
+        return 0
+
+    else:
+        json_response = response.json()
+
+        error = json_response.get('error')
+        if error:
+            print(f'VK response error: {error}')
+            return 0
+
+        user_response = json_response.get('response')
+        if not user_response:
+            log.error('VK response error: block "respose" is missing')
+            return 0
+
+        user = user_response[0]
+        if 'online' not in user:
+            log.error('VK response error: "online" field is missing')
+            return 0
+
+        return user['online']
 
 
-def sms_sender(sms_text):
-    client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_TOKEN'))
-
+def sms_sender(client, sms_text):
     message = client.messages.create(
             body=sms_text,
             from_=os.getenv('NUMBER_FROM'),
@@ -35,9 +72,19 @@ def sms_sender(sms_text):
 
 
 if __name__ == '__main__':
+    log.info('Starting application')
+
     vk_id = input('Введите id ')
     while True:
+        client = Client(
+            os.getenv('TWILIO_ACCOUNT_SID'),
+            os.getenv('TWILIO_TOKEN')
+        )
         if get_status(vk_id) == 1:
-            sms_sender(f'{vk_id} сейчас онлайн!')
+            sms_sender(client, f'{vk_id} сейчас онлайн!')
             break
+
+        log.info('User is not online. Try after 5 sec')
         time.sleep(5)
+
+    log.info('Ending application')
